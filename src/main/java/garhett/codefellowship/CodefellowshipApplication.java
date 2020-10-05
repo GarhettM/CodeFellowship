bcba35965809792b5034bbd96aff9e8be7e7a108
Garhett Morgan <32fossil@gmail.com>
2020-10-05 15:56:56 -0700

    Initial commit

---
+++ src/main/java/garhett/codefellowship/CodefellowshipApplication.java
@@ -0,0 +1,13 @@
+package garhett.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipApplication.class, args);
+	}
+
+}
